8111c4811d1e5fbcb06ca1da74116de79b4bae62
nroudeau <nroudeau@gmail.com>
2025-12-07 22:38:46 +0100

    QlearningAgent v1 -> must change reward system later

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,6 @@ from .base_agent import BaseAgent
 from .greedy_path_agent import GreedyPathAgent
 from .random_agent import RandomAgent
 from .my_agent import MyAgent
+from .q_learning_agent import QlearningAgent
 
-__all__ = ['BaseAgent', 'GreedyPathAgent', 'RandomAgent', 'MyAgent']
+__all__ = ['BaseAgent', 'GreedyPathAgent', 'RandomAgent', 'MyAgent', 'QlearningAgent']
